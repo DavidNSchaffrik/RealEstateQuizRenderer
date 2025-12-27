c17f930a6999f49f902c5b5ffd662e8af4f4b383
David <223673370+DavidNSchaffrik@users.noreply.github.com>
2025-12-28 03:50:52 +0800

    feat(rightmove) add RightMoveListing.scrapeimageURL method to fetch media url.

--- main.py
+++ main.py
@@ -33,4 +33,18 @@ class RightMoveListing:
         content = tree.xpath("//meta[@name='twitter:description']/@content")
         m = re.search(r'\bin\s+(.*?)\s+for\s+£', content[0] if content else "")
         return m.group(1) if m else None
+    
+
+    async def scrapeimageURL(self):
+        url = self.url.split("#", 1)[0]
+        headers = {"User-Agent": "Mozilla/5.0"}
+
+        async with httpx.AsyncClient(headers=headers, timeout=20, follow_redirects=True) as client:
+            r = await client.get(url)
+            r.raise_for_status()
+
+        tree = html.fromstring(r.text)
+        return tree.xpath("//meta[@name='twitter:image:src']/@content")
+        
+
 
